527d40cf9cdae6add633f339e619a6f1c88ef063
emincan <emincansumer@hotmail.com>
2014-04-22 20:57:53 +0300

    added share form

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,6 +4,11 @@
 
 <div class="content-padded">
     <h2 class="text-center">Konumunuzu paylaşmanın en kolay yolu</h2>
+    <p class="text-center">Size verilen kodu girerek, başka bir kullanıcının paylaşımına katılabilirsiniz.</p>
+    <form action="" method="post">
+        <input type="text" placeholder="Kod...">
+        <button class="btn btn-positive btn-block" type="submit">Katıl</button>
+    </form>
 </div>
 
 <div class="bar bar-standard bar-footer">
