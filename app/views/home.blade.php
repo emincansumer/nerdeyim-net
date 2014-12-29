2715782ff56e9e9ff4dd8d330a942e14c167ff32
Emin Can Sumer <emincansumer@hotmail.com>
2014-12-29 11:31:36 +0200

    text changes

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <div class="content-padded">
-    <h2 class="text-center">Konumunuzu paylaşmanın en kolay yolu</h2>
-    <p class="text-center">Size verilen kodu girerek, başka bir kullanıcının paylaşımına katılabilirsiniz.</p>
+    <h2 class="text-center">Nerede olduğunuzu tarif etmenin en kolay yolu</h2>
+    <p class="text-center">Size verilen kodu girerek, diğer kullanıcının nerede olduğunu görebilir, yol tarifini öğrenebilirsiniz.</p>
     <form action="" method="post" class="main-form">
         <input type="text" id="code" placeholder="Kod...">
         <button class="btn btn-positive btn-block" type="submit">Katıl</button>
